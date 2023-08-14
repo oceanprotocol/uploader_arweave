345b39750869e2479d9018b943bb4bcceec529f1
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-08-14 13:00:43 +0300

    Adding function for dynamically estimating the gas needed

--- app/controllers/gasEstimate.js
+++ app/controllers/gasEstimate.js
@@ -1,16 +1,48 @@
 const ethers = require('ethers');
 
-/**
- * Sum of gas estimates for:
- * 1. transferFrom
- * 2. unwrap
- * 3. send native
- * 4. wrap
- * 5. transfer
- *
- * This number empirically derived from:
- * https://github.com/MantisClone/arweave-upload/blob/c119111744c69658af33067cde53147087340978/scripts/estimateGas.js
- */
-const gasEstimate = ethers.BigNumber.from(158751);
+const estimateGas = async (
+  providerUrl,
+  tokenAddress,
+  bundlrAddress,
+  priceWei
+) => {
+  console.log(`provider URL = ${providerUrl}`);
 
-module.exports = { gasEstimate };
+  let provider;
+  try {
+    provider = ethers.getDefaultProvider(providerUrl);
+    console.log(`network = ${JSON.stringify(await provider.getNetwork())}`);
+  } catch (err) {
+    console.error(`Error occurred while getting network info. ${err?.name}: ${err?.message}`);
+    return;
+  }
+
+  const serverWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const userWallet = new ethers.Wallet(process.env.TEST_PRIVATE_KEY, provider);
+  const token = new ethers.Contract(tokenAddress, abi, serverWallet);
+
+  const estimates = [
+    { method: () => token.estimateGas.transferFrom(userWallet.address, serverWallet.address, priceWei), name: 'transferFrom' },
+    { method: () => token.estimateGas.withdraw(priceWei), name: 'withdraw' },
+    { method: () => serverWallet.estimateGas({ to: bundlrAddress, value: priceWei }), name: 'send ETH' },
+    { method: () => token.estimateGas.deposit(priceWei), name: 'deposit' },
+    { method: () => token.estimateGas.transfer(userWallet.address, priceWei), name: 'transfer' },
+  ];
+
+  let gasEstimate = ethers.BigNumber.from(0);
+  for (const { method, name } of estimates) {
+    try {
+      const estimate = await method();
+      console.log(`${name}Estimate = ${estimate}`);
+      gasEstimate = gasEstimate.add(estimate);
+    } catch (err) {
+      console.error(`Error occurred while estimating ${name} gas cost. ${err?.name}: ${err?.message}`);
+      return;
+    }
+  }
+
+  console.log(`gasEstimate = ${gasEstimate}`);
+  return gasEstimate;
+};
+
+module.exports = { estimateGas };
