07225d37a73b08c64be63d819a0134a849316f86
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-08-18 11:21:32 +0300

    getHistory reponse changes.

--- app/controllers/quote.controller.js
+++ app/controllers/quote.controller.js
@@ -496,7 +496,12 @@ exports.getHistory = async (req, res) => {
 			return;
 		}
 		console.log(`${req.path} response: 200: ${JSON.stringify(history)}`);
-		res.send(history);
+		let filteredHistory = history.filter((elem) => elem.userAddress === userAddress);
+		//Accept the first 25 elements
+		if (filteredHistory.length > 25) {
+			filteredHistory = filteredHistory.slice(0, 25)
+		}
+		res.send(filteredHistory);
 	}
 	catch(err) {
 		errorResponse(req, res, err, 500, "Error occurred while looking up status.");
