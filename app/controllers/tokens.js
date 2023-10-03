1606daab3bdc29e50bfb6a91f1d83ab987ef38b1
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-10-03 18:03:59 +0300

    Update token name on matic network

--- app/controllers/tokens.js
+++ app/controllers/tokens.js
@@ -81,7 +81,7 @@ const mainnetTokens = [
   {
     bundlrName: 'matic',
     chainId: 137,
-    symbol: 'MATIC',
+    symbol: 'WMATIC',
     providerUrl: 'https://polygon-rpc.com',
     tokenAddress: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
     confirms: 30
